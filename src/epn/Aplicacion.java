aaad54e11868871ee6be07c19e32021685b05322
USRSIG <USRSIG>
2017-01-13 12:57:29 -0500

    busqueda completa

--- src/epn/Aplicacion.java
+++ src/epn/Aplicacion.java
@@ -3,8 +3,30 @@ package epn;
 public class Aplicacion {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ListaEnlazadaS lista= new ListaEnlazadaS();
+		lista.insertarInicio("prueba1");
+		lista.insertarInicio("prueba2");
+		lista.insertarFinal("prueba3");
+		lista.insertarFinal("prueba4");
+		lista.insertarInicio("prueba5");
+		lista.imprimir();
+		try {
+			System.out.println("El nodo contiene la informacion: "+lista.buscarNodo("prueba2").getElemento());
+			}
+		catch(NullPointerException e){
+			System.out.println("No se encuentra en la lista");
+		}
+		try {
+		System.out.println("El nodo de esa posicion contiene: "+lista.buscarNodoPos(4).getElemento());
+		}
+		catch(NullPointerException e){
+			System.out.println("No se encuentra en la lista");
+		}
+		lista.insertarInicio("0");
+		lista.imprimir();
+		lista.insertarFinal("100");
+		lista.imprimir();
+		System.out.println(lista.buscarNodoPos(4).getElemento());
 	}
 
 }
--- src/epn/ListaEnlazadaS.java
+++ src/epn/ListaEnlazadaS.java
@@ -1,7 +1,6 @@
 package epn;
 
 public class ListaEnlazadaS {
-	
 	protected Nodo cabeza;
 	protected long tamaño;
 	
@@ -10,30 +9,71 @@ public class ListaEnlazadaS {
 		this.tamaño = 0;
 	}
 	
-	public void insertarFinal(String elemento){
-		Nodo aux= new Nodo(elemento);
-		Nodo actual= inicio;
-		if(vacio()){
-			inicio= aux;
-		}
-		else{
-			while(actual.getEnlace()!=null){
-				actual= actual.getEnlace();
-		}
-			actual.setEnlace(aux);
-		}
-		tam++;
+	public ListaEnlazadaS(long tamaño) {
+		this.tamaño=tamaño;
+		this.cabeza=null;
+	}
+
+	public boolean vacio(){
+		return cabeza==null;
 	}
 	
-	public void insertarInicio(String elemento){
-	
-		Nodo actual=inicio;
+	public void insertarInicio(String elemins){
 		if(vacio()){
-			inicio= new Nodo(elemento);
+		cabeza= new Nodo(elemins);
 		}
 		else{
-			inicio= new Nodo(elemento, actual);
+		cabeza= new Nodo(elemins,cabeza);
 		}
 	}
 	
+	public void insertarFinal(String elemins){
+		if(vacio()){
+			cabeza=new Nodo(elemins);
+		}
+		else{
+			Nodo p=cabeza;
+		while(p.getSiguiente()!=null){
+			p=p.getSiguiente();
+		}
+		Nodo q= new Nodo(elemins);
+		p.setSiguiente(q);
+		}
+	}
+	
+	public Nodo buscarNodo(String dato){
+		Nodo buscador;
+		for(buscador=cabeza;buscador!=null;buscador=buscador.getSiguiente()){
+			if(dato==buscador.getElemento()){
+				return buscador;
+			}
+		}
+			return null;
+	}
+	
+	public Nodo buscarNodoPos(int ref){
+		Nodo buscador;
+		int indice=1;
+		for(buscador=cabeza;(buscador!=null)&&(indice<=ref);indice++ /*&& (buscador=buscador.getSiguiente()*/){
+			if(indice==ref){
+				return buscador;
+			}
+			buscador=buscador.getSiguiente();
+		}
+		return null;
+	}
+	
+	public void imprimir(){
+		if(vacio()){
+			System.out.println("Vacia \n");
+			return;
+		}
+		
+		Nodo actual=cabeza;
+		while(actual!=null){
+			System.out.println(actual.getElemento());
+			actual=actual.getSiguiente();
+		}
+		System.out.println("\n");
+	}
 }
